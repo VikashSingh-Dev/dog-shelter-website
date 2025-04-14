cb14c502bfec8ad5b3cfba01cd6b7292296711cd
VikashSingh-Dev <vikashsinghhvc@gmail.com>
2025-04-14 17:52:15 -0400

    Firebase and name changes
    firebase deployment and name changes for that

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATwsq8hyn3qo-t7ro41vT5ACS-2XaO1bY",
+    authDomain: "pawsomerescues-info1601-dev.firebaseapp.com",
+    projectId: "pawsomerescues-info1601-dev",
+    storageBucket: "pawsomerescues-info1601-dev.firebasestorage.app",
+    messagingSenderId: "370193559417",
+    appId: "1:370193559417:web:0a919b4e60202e3fbe09a8"
+  };
+
+export default firebaseConfig;
