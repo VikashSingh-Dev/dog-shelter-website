a00f4d698f22c8cd3aadf2cb5c796fc6bb51ecde
Trishelle Lobin <trishellelobin2005@gmail.com>
2025-04-16 09:53:02 -0400

    donation form js, css and html

--- public/Home.js
+++ public/Home.js
@@ -2,3 +2,30 @@ window.addEventListener("scroll", function() {
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function showPopup() {
+    document.getElementById("donatePopup").style.display = "block";
+}
+
+
+function submitDonation() {
+    var amount = document.getElementById("cashAmount").value;
+
+    if (amount === "") {
+        alert("Please enter an amount!");
+    } else {
+        document.getElementById("formFields").style.display = "none";
+        document.getElementById("thankYou").style.display = "block";
+    }
+}
+
+function closePopup() {
+    document.getElementById("donatePopup").style.display = "none";
+
+    document.getElementById("donorName").value = "";
+    document.getElementById("checkNumber").value = "";
+    document.getElementById("cashAmount").value = "";
+
+    document.getElementById("formFields").style.display = "block";
+    document.getElementById("thankYou").style.display = "none";
+}
