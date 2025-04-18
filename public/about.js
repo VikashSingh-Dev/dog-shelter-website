398b2f2c83b3e13090e341d47b4c64bbe710032f
Trishelle Lobin <trishellelobin2005@gmail.com>
2025-04-18 14:05:40 -0400

    separated all js, css and html into respective files

---
+++ public/about.js
@@ -0,0 +1,47 @@
+
+   
+const apiUrl = 'https://dog.ceo/api/breeds/image/random/20';
+let characterData = [];
+let currentIndex = 0;
+
+async function fetchData() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        characterData = data.message;
+
+        if (characterData.length > 0) {
+            displayCharacter(currentIndex);
+            startSlideshow();  
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayCharacter(index) {
+    const dog = characterData[index];
+    const itemDetails = document.querySelector('#item-details');
+    itemDetails.innerHTML = `
+        
+        <img src="${dog}">
+    `;
+}
+
+function changeCharacter(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = characterData.length - 1;  
+    } else if (currentIndex >= characterData.length) {
+        currentIndex = 0;  
+    }
+
+    displayCharacter(currentIndex);
+}
+function startSlideshow() {
+    setInterval(function() {
+        changeCharacter(1);  
+    }, 3000);  
+}
+
+fetchData(); 
